ceb54f81acd6d1951c046fe0f42851bb2a36852c
VANSHI1194 <pansarivanshi@gmail.com>
2023-07-19 22:12:48 +0530

    Todo List Final

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TodoList from './TodoList';
+import './index.css'
+//TODO LIST
+function App() {
+  
+  return (
+    <div className='app'>
+      {/* <Todo/> */}
+      <TodoList/>
+    </div>
+  )
+}
+
+export default App;
